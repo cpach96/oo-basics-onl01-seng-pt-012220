3b750c790d13a00c382997a80656c3b60f6aa0fb
Curtis pachtinger <cpach96@gmail.com>
2020-02-08 01:05:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader 
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+  puts "the shoe has been repaired"
+  
+  end
+  
   
   
 end
